9fda2b8003bf4a0859d65c0674e90c246e19ebb9
Elena Lemeskina <gogulinaelena@yandex.ru>
2022-10-22 19:38:01 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(4, 5));
+        System.out.println(calc.mult(5, 2));
+        System.out.println(calc.pow(3, 10));
+    }
+}
